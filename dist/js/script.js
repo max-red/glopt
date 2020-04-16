a517d1704248fd5221fb6d5d882bc2a8f128ee4a
Maksim <maxred1982@gmail.com>
2020-04-16 14:32:46 +0300

    burger

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
         dotsClass: 'slick-dots'
     });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('.body'),
+        menu = document.querySelector('.header__menu'),
+        menuItem = document.querySelectorAll('.header__item'),
+        burger = document.querySelector('.header__burger');
+
+    burger.addEventListener('click', () => {
+        body.classList.toggle('body_active');
+        burger.classList.toggle('header__burger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            body.classList.toggle('body_active');
+            burger.classList.toggle('header__burger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    });
+});
